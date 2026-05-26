e53ff8b82d359fe09a6eb46a208c17cf9dd2d6e2
Hemanth <hemanthvhb@gmail.com>
2026-05-26 16:47:28 +0530

    restructured euclidean code

--- math_engine.py
+++ math_engine.py
@@ -1,14 +1,15 @@
 import numpy as np
 
-a=np.array([[1,2,3],
+def euclidean_dist(a,b):
+    c=np.array(a)-np.array(b)
+    d=np.sum(c**2)
+    return np.sqrt(d)
+if __name__ == "__main__":
+    
+    a=[[1,2,3],
         [4,5,6],
-        [7,8,9]])
-b=np.array([[1,1,1],
+        [7,8,9]]
+    b=[[1,1,1],
             [1,1,1],
-            [1,1,1]])
-c=a-b
-c=c**2
-ans=np.sum(c)
-print(c)
-print(ans)
-print(np.sqrt(ans))
+            [1,1,1]]
+    print(euclidean_dist(a,b))
