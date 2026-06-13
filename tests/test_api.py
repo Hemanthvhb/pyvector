d7941ba50f4203331e8a6e1809533076d0c9bb6f
Hemanth <hemanthvhb@gmail.com>
2026-06-13 17:29:35 +0530

    after test

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,3 +2,15 @@ from fastapi.testclient import TestClient
 from main import app
 
 client = TestClient(app)
+
+def test_insert_api():
+    response=client.post("/insert",json={
+            "vector":[1.0,2.0,3.0],
+            "payload": {"name":"cat"}
+    })
+
+    assert response.status_code==200
+    assert response.json()["message"] == "vector inserted successfully"
+
+def test_search_api():
+    pass
